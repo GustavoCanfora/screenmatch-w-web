08a911b78969b0babc8958c5b2f17508987b5b9c
unknown <gustavocanf@gmail.com>
2024-09-03 16:24:52 -0300

    Demo project Spring

---
+++ screenmatch2.0/src/main/java/br/com/alura/screenmatch/main/ScreenmatchApplication.java
@@ -0,0 +1,31 @@
+package br.com.alura.screenmatch.main;
+
+import br.com.alura.screenmatch.model.DadosSeries;
+import br.com.alura.screenmatch.service.ConsumoAPI;
+import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(ScreenmatchApplication.class, args);
+
+
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoAPI consumo = new ConsumoAPI();
+		var json = consumo.obterDados("https://www.omdbapi.com/?t=Black%20Mirror&apikey=9b1a43a6");
+		System.out.println(json);
+
+		ConverteDados conversor = new ConverteDados();
+		DadosSeries dados = conversor.obterDados(json, DadosSeries.class);
+		System.out.println(dados);
+	}
+}
